6ea36a61c3693717c338f843b46e3debc0bdb59e
Guille <enrique2003junior@gmail.com>
2022-07-12 23:38:33 -0500

    Agregando cambios

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-//Si lleva más de 10 productos del mismo tipo se le da el decimo gratis todos los días
-//Si lleva cada uno de los tipos de productos se da un descuento del 10%
+//Si lleva más de 10 productos del mismo tipo se le da el decimo gratis todos los días, ESTO SERÁ IMPLÍCITO SIN AVISAR
+//Si lleva cada uno de los tipos de productos se da un descuento del 10%, ESTO SERÁ IMPLÍCITO SIN AVISAR
 //Los L, J y V tiene descuento del 20%.
 
+//Base de datos
 
-//funcion para imprimir los resultados
-// const imprimir = (nombre) => {
-//     if (nombreProductos.includes(nombre)) {
-//         return `El producto Hamburguesa ${nombre}, tiene como precio final $${precioProductos[nombreProductos.indexOf(nombre)]}`
-//     };
-// };
+const dias = ["L", "J", "V"]; //días de descuento
+class InfoDeProductos {
+    constructor(nombre, precioUnitario, cantidad) {
+        this.nombre = nombre;
+        this.precioUnitario = precioUnitario;
+        this.cantidad = cantidad;
+        this.precioPorCantidad = 0; 
+    };
+};
+
+const infoDeProductos = [];
+
+infoDeProductos.push(new InfoDeProductos("papas", 1, 0));
+infoDeProductos.push(new InfoDeProductos("cebolla", 1.2, 0));
+infoDeProductos.push(new InfoDeProductos("calabacin", 2.1, 0));
+infoDeProductos.push(new InfoDeProductos("fresas", 0.6, 0));
+
+//variable
+let precioFinal = 0;
 
 //funcion para filtrar productos 
 function filtrarProductos(filtro) {
@@ -17,38 +31,28 @@ function filtrarProductos(filtro) {
     if (filtracion.length === 0) {
         alert("Filtración fallida");
     } else {
-        const filtraciones = filtracion.map(el => el.nombre + "\n")
+        const filtraciones = filtracion.map(el => el.nombre + "\n");
         alert(`Estos son los productos filtrados \n${filtraciones.join("")}`);
     };
 };
 
 //funciones para el descuento de días
 //verificar el día para realizar el descuento 
-const verificarTipo = (nombre, array, dia) => {
+const verificarDia = (objeto, dia) => {
     if (dias.includes(dia)) {
-        const elementoModificado = array.find(element => element.nombre === nombre);
-        elementoModificado.precioPorCantidad *= 1000;
+        alert("Es día de descuentos :) \n20% menos en tu compra total");
+        objeto.precioPorCantidad *= .8;
+    } else {
+        alert("No es día de descuentos :(");
     };
 };
 
-//funcion para contar los productos
-// const contadorProductos = (i) => {
-//     let contador = cantidadProductos[i];
-//     contador += 1;
-//     cantidadProductos[i] = contador;
-// };
-
 //funcion para determinar el precio con descuento "de cada 10 te llevas uno gratis"
-// const retornarOferta = (i) => {
-//     if (cantidadProductos[i] >= 10) {
-//         return precioUnitario[i] * cantidadProductos[i] - ((cantidadProductos[i] - (cantidadProductos[i] % 10)) / 10) * precioUnitario[i];
-//     } else {
-//         return precioUnitario[i] * cantidadProductos[i];
-//     }
-// }
-
-//variables
-let valorWhile;//valor para el ciclo do ..while
+const retornarOferta = (objeto) => {
+    if (objeto.cantidad >= 10) {
+        objeto.precioPorCantidad = objeto.precioUnitario * objeto.cantidad - ((objeto.cantidad - (objeto.cantidad % 10)) / 10) * objeto.precioUnitario;
+    };
+};  
 
 alert(
     "Papas: $1 \nCebolla: $1.2 \nCalabacin: $2.1 \nFresas $0.6 \nSi llevas por lo menos 1u. de cada producto te damos un descuento del 10% \nPor cada 10 productos del mismo tipo se le da el décimo totalmente gratis \nLos Lunes, Jueves y Viernes tenemos descuento del 20% adicional"
@@ -57,19 +61,45 @@ alert(
 //do ..while
 do {
     let preguntaInicial = prompt("¿Qué tipo de producto desea llevar?\nEscriba el nombre o letra para filtrar").toLowerCase();
-    filtrarProductos(preguntaInicial)
+    filtrarProductos(preguntaInicial);
 
     let preguntaSecundaria = prompt("De acuerdo a su filtración, ¿qué producto desea llevar?").toLowerCase();
-    for (const el of infoDeProductos) {
-        if (preguntaSecundaria === el.nombre) {
-            let preguntaTerciaria = parseInt(prompt("¿Cuántos deseas llevar?"));
-            el.cantidad += preguntaTerciaria;
-            el.precioPorCantidad = el.cantidad * el.precioUnitario
-            alert(el.nombre + el.cantidad + el.precioPorCantidad);
-            break;
-        }
-    }
 
-    console.log("xd");
-    valorWhile = prompt("¿Desea continuar agregando productos?\n'y' para continuar \n'cualquier tecla' para salir").toLowerCase();
-} while(valorWhile !== "y");
+    //Ciclo for ..of para tener el elemento que en filtra
+    for (const el of infoDeProductos) {
+        //Verificamos que este el producto
+        if (preguntaSecundaria === el.nombre) {
+            const preguntaCantidad = prompt("¿Cuántos deseas llevar?");
+            //Verificamos si es una cantidad válida
+            if (preguntaCantidad >= 1){
+                el.cantidad += preguntaCantidad*1;
+                el.precioPorCantidad += (el.cantidad * el.precioUnitario);
+                alert(el.nombre + "\nCantidad a llevar: " + el.cantidad + "\nPrecio Total de este producto: " + el.precioPorCantidad);
+                retornarOferta(el);
+
+                //Verificamos el días para dar descuento
+                const preguntaDias = prompt("¿Qué día es hoy? \nLunes: 'L' \nJueves: 'J' \nViernes: 'V'").toUpperCase();
+                verificarDia(el, preguntaDias);
+                break;
+            } else {
+                alert("Ingrese un valor númerico")
+                break;
+            };
+        };
+    };
+
+} while(confirm("¿Desea continuar agregando productos?"));
+
+// Verificamos si compro por lo menos un producto de cada tipo para el descuento del 10%
+const descuento = infoDeProductos.find(el => el.cantidad === 0);
+if(descuento === undefined){
+    for (obj of infoDeProductos){
+        obj.precioPorCantidad *= .9;
+    };
+};
+
+console.log(infoDeProductos);
+for (const el of infoDeProductos){
+    precioFinal += el.precioPorCantidad;
+};
+alert(`El precio total es:  ${precioFinal.toFixed(2)}`);
